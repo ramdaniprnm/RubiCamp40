e0b001709c060898d7723279f14c2c2cc4c3242c
ramdaniprnm <ramdanipurnama539@gmail.com>
2024-09-19 09:30:38 +0700

    Commit Challenge 01 - 08

---
+++ C08.js
@@ -0,0 +1,15 @@
+
+function pola(str) {
+    const [polaA, polaB] = str.split('=');
+    for (let i = 0; i < 10; i++) {
+        let cariPolaA = polaA.replace('#', i);
+        for (let j = 0; j < 10; j++) {
+            let cariPolaB = polaB.replace('#', j);
+            const result = new Function(`return ${cariPolaA}`)();
+            if (result == cariPolaB) return [i, j]
+        }
+        
+    }
+}
+console.log(pola("42#3 * 188 = 80#204"));
+console.log(pola("8#61 * 895 = 78410#5"));
