72b2d2be28249dd40f6d9791c68c9cd8aaf7d903
Ramdani <ramdanipurnama539@gmail.com>
2024-10-08 07:18:00 +0700

    commit C15 rev2

--- C15/univ.sql
+++ C15/univ.sql
@@ -1,6 +1,6 @@
 -- no 1
 
-SELECT DISTINCT mahasiswa.nim, mahasiswa.nama, mahasiswa.tgl_lahir, mahasiswa.alamat FROM mahasiswa INNER JOIN jurusan ON jurusan.id_jurusan = jurusan.id_jurusan; 
+SELECT DISTINCT mahasiswa.nim, mahasiswa.nama, mahasiswa.tgl_lahir, mahasiswa.alamat, jurusan.id_jurusan, jurusan.nama_jurusan FROM mahasiswa INNER JOIN jurusan ON jurusan.id_jurusan = jurusan.id_jurusan; 
 
 NI01|Rahmat|2006-03-14|JL.Waluh
 NI02|Rohimat|2000-06-16|JL.Kawal
@@ -17,14 +17,14 @@ NI01|Rahmat|2006-03-14|JL.Waluh|IDJ01|18
 NI03|Isnain|2005-03-14|JL.Jalan|IDJ02|19
 NI02|Rohimat|2000-06-16|JL.Kawal|IDJ02|24
 
- SELECT *,        
+SELECT *,        
 (strftime('%Y', 'now') - strftime('%Y', tgl_lahir)) AS umur
-FROM mahasiswa
-ORDER BY umur DESC; 
+FROM mahasiswa 
+WHERE umur < 20 
+ORDER BY umur; 
 
-NI02|Rohimat|2000-06-16|JL.Kawal|IDJ02|24
-NI03|Isnain|2005-03-14|JL.Jalan|IDJ02|19
 NI01|Rahmat|2006-03-14|JL.Waluh|IDJ01|18
+NI03|Isnain|2005-03-14|JL.Jalan|IDJ02|19
 
 -- no 3 y
 
@@ -37,40 +37,94 @@ NI01|Rahmat|B
 
 -- no 4
 
-SELECT * FROM matakuliah
-WHERE sks > 10;
-id04|Lil Jonh|11
+SELECT mahasiswa.nim, mahasiswa.nama, sum(matakuliah.sks) AS total_sks 
+FROM assignment 
+INNER JOIN mahasiswa ON assignment.nim = mahasiswa.nim 
+INNER JOIN matakuliah ON assignment.id_matakuliah = matakuliah.id_matakuliah 
+GROUP BY mahasiswa.nim
+HAVING total_sks > 10; 
+
+NI01|Rahmat|11
 
 -- no 5
 
-SELECT * FROM mahasiswa JOIN matakuliah ON matakuliah.id_matakuliah = matakuliah.id_matakuliah WHERE matakuliah.nama = 'Data Mining';
+SELECT * , matakuliah.nama 
+FROM assignment 
+JOIN matakuliah ON assignment.id_matakuliah = matakuliah.id_matakuliah 
+WHERE matakuliah.nama LIKE '%Data Mining%';
 
-NI01|Rahmat|2006-03-14|JL.Waluh|IDJ01|id03|Data Mining|3
-NI02|Rohimat|2000-06-16|JL.Kawal|IDJ02|id03|Data Mining|3
-NI03|Isnain|2005-03-14|JL.Jalan|IDJ02|id03|Data Mining|3
+5|NI01|id03|NP45|D|id03|Data Mining|3|Data Mining
 
 -- no 6
 
-SELECT dosen.*, COUNT(DISTINCT mahasiswa.nim) AS jumlah_mahasiswa FROM assignment JOIN mahasiswa ON assignment.nim 
-= mahasiswa.nim JOIN dosen ON assignment.nip = dosen.nip GROUP BY dosen.nip;    
+SELECT dosen.*, COUNT(DISTINCT mahasiswa.nim) AS jumlah_mahasiswa 
+FROM assignment 
+JOIN mahasiswa ON assignment.nim = mahasiswa.nim 
+JOIN dosen ON assignment.nip = dosen.nip 
+GROUP BY dosen.nip; 
 
 NP45|Pak Lawd|1  
-NP66|Pak Muh|1
+NP66|Pak Muh|2
 
 -- no 7
 
-SELECT DISTINCT mahasiswa.* FROM mahasiswa ORDER BY mahasiswa.tgl_lahir ASC; 
+SELECT *, 
+(strftime('%Y', 'now') - strftime('%Y', tgl_lahir)) AS umur
+FROM mahasiswa
+ORDER BY umur ASC;
 
-NI02|Rohimat|2000-06-16|JL.Kawal|IDJ02
-NI03|Isnain|2005-03-14|JL.Jalan|IDJ02
-NI01|Rahmat|2006-03-14|JL.Waluh|IDJ01
+NI01|Rahmat|2006-03-14|JL.Waluh|IDJ01|18
+NI03|Isnain|2005-03-14|JL.Jalan|IDJ02|19
+NI02|Rohimat|2000-06-16|JL.Kawal|IDJ02|24
 
-SELECT DISTINCT mahasiswa.* FROM mahasiswa ORDER BY mahasiswa.tgl_lahir DESC;
+SELECT *, 
+(strftime('%Y', 'now') - strftime('%Y', tgl_lahir)) AS umur
+FROM mahasiswa
+ORDER BY umur DESC;
 
-NI01|Rahmat|2006-03-14|JL.Waluh|IDJ01
-NI03|Isnain|2005-03-14|JL.Jalan|IDJ02
-NI02|Rohimat|2000-06-16|JL.Kawal|IDJ02
+NI02|Rohimat|2000-06-16|JL.Kawal|IDJ02|24
+NI03|Isnain|2005-03-14|JL.Jalan|IDJ02|19
+NI01|Rahmat|2006-03-14|JL.Waluh|IDJ01|18
 
 
 -- no 8
 
+SELECT 
+       mahasiswa.nama,
+       mahasiswa.nim,
+       mahasiswa.tgl_lahir,
+       mahasiswa.alamat,
+       jurusan.id_jurusan,
+       jurusan.nama_jurusan,
+       dosen.nip,
+       dosen.nama,
+       matakuliah.nama,
+       matakuliah.id_matakuliah,
+       assignment.nilai
+FROM assignment
+JOIN mahasiswa ON assignment.nim = mahasiswa.nim
+JOIN matakuliah ON assignment.id_matakuliah = matakuliah.id_matakuliah
+JOIN jurusan ON mahasiswa.id_jurusan = jurusan.id_jurusan
+JOIN dosen ON assignment.nip = dosen.nip  
+WHERE assignment.nilai IN ('D', 'E');
+
+SELECT 
+       mahasiswa.nama,
+       mahasiswa.nim,
+       mahasiswa.tgl_lahir,
+       mahasiswa.alamat,
+       jurusan.id_jurusan,
+       jurusan.nama_jurusan,
+       dosen.nip,
+       dosen.nama,
+       matakuliah.nama,
+       matakuliah.id_matakuliah,
+       assignment.nilai
+FROM assignment, mahasiswa, matakuliah, jurusan, dosen
+WHERE 
+      assignment.nim = mahasiswa.nim      
+      AND assignment.id_matakuliah = matakuliah.id_matakuliah
+      AND mahasiswa.id_jurusan = jurusan.id_jurusan
+      AND assignment.nip = dosen.nip
+      AND assignment.id_matakuliah = matakuliah.id_matakuliah
+      AND assignment.nilai IN ('D', 'E');
